93366a93a2bbf4699551d796fc228b7090f85643
Tha_UI_Guy <adeniran1059@gmail.com>
2018-05-11 09:45:29 +0100

    Add files via upload

---
+++ carousel.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-carousel>
+        <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+  export default {
+    components: {
+      carousel
+    },
+    data () {
+      return {
+        items: [
+          {
+            src: '/static/doc-images/carousel/squirrel.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/sky.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/bird.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
